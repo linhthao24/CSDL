d95fb33b92d4451fa83898eb3612e1f0d189ba8c
linhthao24 <113225411+linhthao24@users.noreply.github.com>
2023-10-31 09:19:55 +0700

    Add files via upload

--- insert_data.sql
+++ insert_data.sql
@@ -1,11 +1,43 @@
-INSERT INTO csdl.student (MSV, Full_name, Date_of_birth, Gender, Address, Subject_studied, Major, Day_admission, Role, Class)
+INSERT INTO csdl.student (MSV, Full_name, Date_of_birth, Gender, Address, Major, Day_admission, Role, Class, credit_has_learned)
 VALUES
-  ('MSV1', 'Nguyen Van A', '1990-01-01', 1, '123 ABC Street', NULL, 'Major A', '2020-01-01', 1, 'Class A'),
-  ('MSV2', 'Tran Thi B', '1991-02-02', 0, '456 XYZ Street', NULL, 'Major B', '2020-01-01', 2, 'Class B'),
-  ('MSV3', 'Le Van C', '1992-03-03', 1, '789 PQR Street', NULL, 'Major C', '2020-01-01', 1, 'Class A'),
-  ('MSV4', 'Pham Thi D', '1993-04-04', 0, '321 DEF Street', NULL, 'Major A', '2020-01-01', 2, 'Class B'),
-  ('MSV5', 'Hoang Van E', '1994-05-05', 1, '654 GHI Street', NULL, 'Major B', '2020-01-01', 1, 'Class A');
-  INSERT INTO csdl.phone_numbers_student (student_id, phone_number)
+('MSV1', 'John Doe', '2000-01-01', 1, '123 Main St', 'Computer Science', '2021-09-01', 1, 'Class A', 0),
+('MSV2', 'Jane Smith', '1999-05-15', 0, '456 Elm St', 'Electrical Engineering', '2021-09-01', 1, 'Class B', 0),
+('MSV3', 'Alice Johnson', '2001-03-20', 0, '789 Oak St', 'Mechanical Engineering', '2021-09-01', 1, 'Class A', 0),
+('MSV4', 'Bob Williams', '1998-12-10', 1, '101 Pine St', 'Civil Engineering', '2021-09-01', 1, 'Class C', 0),
+('MSV5', 'Eve Davis', '2002-07-05', 0, '202 Cedar St', 'Chemistry', '2021-09-01', 1, 'Class B', 0);
+INSERT INTO csdl.lecturers (MGV, Full_name, Gender, Major, Role, Teaching_subject, Seniority)
+VALUES
+('MGV1', 'Dr. Smith', 1, 'Computer Science', 1, 'Data Structures', 'Senior'),
+('MGV2', 'Prof. Johnson', 0, 'Electrical Engineering', 1, 'Circuit Design', 'Associate'),
+('MGV3', 'Dr. White', 1, 'Mechanical Engineering', 1, 'Thermodynamics', 'Professor'),
+('MGV4', 'Prof. Brown', 1, 'Civil Engineering', 1, 'Structural Analysis', 'Senior'),
+('MGV5', 'Dr. Lee', 0, 'Chemistry', 1, 'Organic Chemistry', 'Associate');
+INSERT INTO csdl.subject (MS, Name_of_subject, Number_of_credits, Major, semester, Prerequisite, Number_of_student)
+VALUES
+('MS1', 'Mathematics', 4, 'Math', 1, '', 0),
+('MS2', 'Physics', 3, 'Physics', 1, '', 0),
+('MS3', 'Computer Programming', 3, 'Computer Science', 2, 'Mathematics', 0),
+('MS4', 'Digital Electronics', 3, 'Electrical Engineering', 2, 'Physics', 0),
+('MS5', 'Mechanics', 4, 'Mechanical Engineering', 3, 'Physics', 0);
+INSERT INTO csdl.subject_studied (student_id, subject_id)
+VALUES
+('MSV1', 'MS1'),
+('MSV1', 'MS3'),
+('MSV2', 'MS1'),
+('MSV2', 'MS4'),
+('MSV3', 'MS2'),
+('MSV4', 'MS2'),
+('MSV4', 'MS3'),
+('MSV5', 'MS1'),
+('MSV5', 'MS4');
+INSERT INTO csdl.register (Student_ID, Subject_ID)
+VALUES
+('MSV1', 'MS1'),
+('MSV2', 'MS1'),
+('MSV3', 'MS2'),
+('MSV4', 'MS3'),
+('MSV5', 'MS4');
+INSERT INTO csdl.phone_numbers_student (student_id, phone_number)
 VALUES
   ('MSV1', '0123456789'),
   ('MSV2', '0123456798'),
@@ -19,13 +51,6 @@ VALUES
   ('MSV3', 'example3@example.com'),
   ('MSV4', 'example4@example.com'),
   ('MSV5', 'example5@example.com');
-  INSERT INTO csdl.lecturers (MGV, Full_name, Gender, Major, Role, Teaching_subject, Seniority)
-VALUES
-  ('MGV1', 'Nguyen Van X', 1, 'Major A', 1, 'Subject A, Subject B', '5 years'),
-  ('MGV2', 'Tran Thi Y', 0, 'Major B', 2, 'Subject C, Subject D', '10 years'),
-  ('MGV3', 'Le Van Z', 1, 'Major C', 1, 'Subject E, Subject F', '3 years'),
-  ('MGV4', 'Pham Thi W', 0, 'Major A', 2, 'Subject G, Subject H', '7 years'),
-  ('MGV5', 'Hoang Van V', 1, 'Major B', 1, 'Subject I, Subject J', '2 years');
   INSERT INTO csdl.phone_numbers_lecturers (lecturers_id, phone_number)
 VALUES
   ('MGV1', '0123456789'),
@@ -40,20 +65,6 @@ VALUES
   ('MGV3', 'example13@example.com'),
   ('MGV4', 'example14@example.com'),
   ('MGV5', 'example15@example.com');
-  INSERT INTO csdl.subject (MS, Name_of_subject, Number_of_credits, Major, semester, Prerequisite, Number_of_student)
-VALUES
-  ('MS1', 'Subject A', 3, 'Major A', 1, 'Prerequisite A', 50),
-  ('MS2', 'Subject B', 4, 'Major A', 2, 'Prerequisite B', 60),
-  ('MS3', 'Subject C', 3, 'Major B', 1, 'Prerequisite C', 70),
-  ('MS4', 'Subject D', 4, 'Major B', 2, 'Prerequisite D', 80),
-  ('MS5', 'Subject E', 3, 'Major C', 1, 'Prerequisite E', 90);
-  INSERT INTO csdl.register (Student_ID, Subject_ID)
-VALUES
-  ('MSV1', 'MS1'),
-  ('MSV2', 'MS2'),
-  ('MSV3', 'MS3'),
-  ('MSV4', 'MS4'),
-  ('MSV5', 'MS5');
   INSERT INTO csdl.manage (Lecturers_ID, Subject_ID)
 VALUES
   ('MGV1', 'MS1'),
@@ -61,8 +72,4 @@ VALUES
   ('MGV3', 'MS3'),
   ('MGV4', 'MS4'),
   ('MGV5', 'MS5');
-  
-  
-  
-  
   
