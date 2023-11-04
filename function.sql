a7a73648d9b77b2a9d6ea632c1bdc3be5a039f9d
linhthao24 <113225411+linhthao24@users.noreply.github.com>
2023-11-04 23:23:02 +0700

    Add files via upload

--- function.sql
+++ function.sql
@@ -1,22 +1,47 @@
--- lecturer saw subject 
-SELECT Subject_ID, Name_of_subject
-FROM csdl.manage
-JOIN csdl.subject ON csdl.manage.Subject_ID = csdl.subject.MS
-WHERE Lecturers_ID = 'MGV1';
--- list of student learn a class
-SELECT Student_ID, Full_name
-FROM csdl.register
-JOIN csdl.student ON csdl.register.Student_ID = csdl.student.MSV
-WHERE Subject_ID = 'MS1';
-
+-- xoa du lieu
+SET SQL_SAFE_UPDATES = 0;
+delete from csdl.emails_student;
+delete from csdl.phone_numbers_student;
+delete from csdl.subject_studied;
+delete from csdl.register;
+delete from csdl.student;
+delete from csdl.lecturers; 
+delete from csdl.emails_lecturers;
+delete from csdl.phone_numbers_lecturers;
+delete from csdl.manage;
+delete from csdl.subject;
+-- show table
+select * from csdl.emails_student;
+select * from csdl.phone_numbers_student;
+select * from csdl.subject_studied;
+select * from csdl.register;
+select * from csdl.student;
+select * from csdl.lecturers; 
+select * from csdl.emails_lecturers;
+select * from csdl.phone_numbers_lecturers;
+select * from csdl.manage;
+select * from csdl.subject;
+-- lecturer
+-- giang vien dang ky day
+SELECT l.MGV, l.Full_name, m.Subject_ID, s.Name_of_subject
+FROM csdl.lecturers AS l
+JOIN csdl.manage AS m ON l.MGV = m.Lecturers_ID
+JOIN csdl.subject AS s ON m.Subject_ID = s.MS;
+-- giang vien xem danh sach sinh vien dang ky lop minh
+SELECT s.MSV, s.Full_name, s.Class
+FROM csdl.student AS s
+JOIN csdl.register AS r ON s.MSV = r.Student_ID
+JOIN csdl.manage AS m ON r.Subject_ID = m.Subject_ID
+JOIN csdl.lecturers AS l ON m.Lecturers_ID = l.MGV
+WHERE l.MGV = 'Lecturer_id';
 -- student
-SELECT csdl.subject.MS, Name_of_subject, Full_name
-FROM csdl.subject
-JOIN csdl.manage ON csdl.subject.MS = csdl.manage.Subject_ID
-JOIN csdl.lecturers ON csdl.manage.Lecturers_ID = csdl.lecturers.MGV
-WHERE csdl.subject.MS NOT IN (
-    SELECT Subject_ID
-    FROM csdl.register
-    WHERE Student_ID = 'MSV1'
-);
--- sign subject
+-- Xem sanh sach cac mon và giang vien dạy mon do
+SELECT s.MS, s.Name_of_subject, s.Number_of_credits, l.Full_name, s.semester
+FROM csdl.subject AS s
+JOIN csdl.manage AS m ON s.MS = m.Subject_ID
+JOIN csdl.lecturers AS l ON m.Lecturers_ID = l.MGV;
+-- xem dach sach mon da dang ky hoc
+SELECT s.MS, s.Name_of_subject
+FROM csdl.subject AS s
+JOIN csdl.register AS r ON s.MS = r.Subject_ID
+WHERE r.Student_ID = 'STUDENT_ID';
